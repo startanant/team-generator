9884ceaa8a490f9495e378c15e8844d3d4ae8c44
Anant S <startanant@gmail.com>
2020-03-01 20:57:18 -0500

    Adds code to prompt manager details

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const inquirer = require( 'inquirer' );
 const fs = require( 'fs' );
-const Employee = require('./employee');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/Manager');
+
+
+const managerPrompts = [
+    {
+        type: "input",
+        message: "Manager Name: ",
+        name: "managerName"
+    },
+    {
+        type: "input",
+        message: "Manager E-mail: ",
+        name: "managerEmail"
+    },
+    {
+        type: "input",
+        message: "Manager Office Number:",
+        name: "managerOffice"
+    }
+];
+
+async function main(){
+    const managerRequest = await inquirer.prompt(managerPrompts);
+    console.log(managerRequest);
+}
+
+main();
