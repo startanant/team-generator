b0ddddfc4656ebfdf35f05b5b1ecd378bbdf8435
Anant S <startanant@gmail.com>
2020-03-01 22:00:17 -0500

    Adds readCard(): Generates Manager card from template manager.html

--- app.js
+++ app.js
@@ -17,14 +17,38 @@ const managerPrompts = [
     },
     {
         type: "input",
-        message: "Manager Office Number:",
+        message: "Manager Office Number: ",
         name: "managerOffice"
     }
 ];
 
+function readCard(cardInfo) {
+    const filename = cardInfo.role.toLowerCase();
+    let html = fs.readFileSync(`templates/${filename}.html`, 'UTF8');
+    
+    // replace any {{ field }} from template and return
+    // regex match: {{ field }}, extract substr: field
+    html = html.replace(/{{([^}]*)}}/g, function(match) {
+        const name = match.substr(3,match.length-6);
+        return cardInfo[name] ? cardInfo[name] : ''; 
+    });
+
+    return html;
+}
+
 async function main(){
     const managerRequest = await inquirer.prompt(managerPrompts);
     console.log(managerRequest);
+
+    const manager = new Manager(managerRequest.managerName, managerRequest.managerEmail, managerRequest.managerOffice);
+
+    //generate html
+    let team = '';
+    team += readCard( manager );
+    console.log(team);
+
+
+
 }
 
 main();
